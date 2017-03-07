a816ba34fa35bc3c75a8396c18d5854086cad652
Jesus Rosas <jesus@ecotechnologic.com.mx>
2017-03-07 01:39:09 -0600

    added views model user and scaffold for events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'home#index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/sessions'
+  }
+
+  authenticated :user do
+    resources :events
+    root 'events#index'
+  end
+
+  unauthenticated :user do
+    root 'home#index'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20170307072821_create_events.rb
@@ -0,0 +1,15 @@
+class CreateEvents < ActiveRecord::Migration[5.0]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.datetime :date
+      t.time :hour
+      t.boolean :cupo
+      t.string :type
+      t.string :price
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
