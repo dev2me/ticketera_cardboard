76728afa16240936eebe9190384c7d0173f5dd47
Jesus Rosas <jesus@ecotechnologic.com.mx>
2017-03-07 01:42:27 -0600

    defining abilities for users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,11 +5,13 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
+    if user.role = "organizador"
+      can :manage, :all
+    elsif user.role = "vendedor"
+      can :read, Ticket
+    elsif user.rola = "portero"
+      can :read, Ticket
+    end
     #
     # The first argument to `can` is the action you are giving the user 
     # permission to do.
